3fb7216d84b23490132e343e72e403ca68e4bce1
Appani Kaushik <appanikaushik@L-YN4LWQHP4F.local>
2023-01-22 22:52:40 +0530

    day4

--- Day4Java/Maps/MapsExplain.java
+++ Day4Java/Maps/MapsExplain.java
@@ -1,19 +1,19 @@
-package Day4;
+package Day4Java.Maps;
 
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeMap;
 
-public class Maps {
-   public static void main(String []args){
-        //myHashMap();
+public class MapsExplain{
+    public static void main(String args[])
+    {
+        // have to complete this code
+        myHashMap();
         myTreeMap();
-   }
-
-   public static void myHashMap(){
+    }
+    public static void myHashMap(){
         Map<Integer,String> map = new HashMap<Integer,String>();
         //Map<Integer,String> map = new LinkedHashMap<Integer,String>();
 
@@ -32,20 +32,15 @@ public class Maps {
         }
    }
    public static void myTreeMap(){
-        Map<Integer,String> treemap = new TreeMap<Integer,String>();
+        Map<Integer,String> map = new LinkedHashMap<Integer,String>();
 
-        treemap.put(101,"pune");
-        treemap.put(102,"chennai");
-        treemap.put(103,"hyderabad");
-        treemap.put(102,"jabalpur");
-        Set<Integer> set = treemap.keySet();
+        map.put(101,"pune");
+        map.put(102,"chennai");
+        map.put(103,"hyderabad");
+        map.put(102,"jabalpur");
+        Set<Integer> set = map.keySet();
         for(Integer key : set){
-            System.out.println(key+" "+treemap.get(key));
+            System.out.println(key+" "+map.get(key));
         }
-        Map<Employee,String> empTree = new TreeMap<Employee,String>();
-        
-
-
    }
-    
 }
